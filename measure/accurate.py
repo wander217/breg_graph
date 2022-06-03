bee417f3645a7368eb3a1ec9633f3383b4f5bd82
thinhtq <Trinhthinh2171999@gmail.com>
2022-06-03 13:48:23 +0700

    initial

--- measure/accurate.py
+++ measure/accurate.py
@@ -13,12 +13,11 @@ class Accurate:
         pred = torch.softmax(pred, dim=1)
         pred_matrix: np.ndarray = np.argmax(pred.cpu().detach().numpy(), axis=1)
         cm: np.ndarray = confusion_matrix(target_matrix, pred_matrix).astype(np.float32)
-        print(len(target_matrix), len(pred_matrix))
         recall: np.ndarray = np.zeros(label_num, dtype=np.float32)
         precision: np.ndarray = np.zeros(label_num, dtype=np.float32)
         h_mean: np.ndarray = np.zeros(label_num, dtype=np.float32)
 
-        for r in range(label_num):
+        for r in range(cm.shape[0]):
             cluster: np.ndarray = np.where(target_matrix == r)[0]
             if cluster.shape[0] != 0:
                 recall[r] = cm[r, r] / float(cluster.shape[0])
