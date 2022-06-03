b35e8951234f23944b632ce40aadce208e8cb2f6
thinhtq <Trinhthinh2171999@gmail.com>
2022-06-03 11:12:50 +0700

    initial

--- dataset/loader.py
+++ dataset/loader.py
@@ -14,11 +14,18 @@ def norm(element: np.ndarray) -> np.ndarray:
     :param element: bounding box coordinate: (n, 8) or (n, 2)
     :return: normed element
     """
+    b, _ = element.shape
     mn = np.min(element, axis=0)
     mx = np.max(element, axis=0)
     normed_point = (element - mn) / (mx - mn)
     normed_element = (normed_point - 0.5) / 0.5
     return normed_element
+
+
+def convert24point(bbox):
+    x1, y1, x2, y2 = bbox
+    return np.array([[x1, y1], [x2, y1],
+                     [x2, y2], [x1, y2]]).flatten()
 
 
 def process(sample: Dict,
@@ -52,12 +59,14 @@ def process(sample: Dict,
     labels = []
     for target in sample[TARGET_KEY]:
         text = alphabet_dict.encode(target[TEXT_KEY])
+        if text.shape[0] > 0:
+            continue
         texts.append(text)
         lengths.append(text.shape[0])
-        # assert text.shape[0] > 0, text.shape[0]
         label: int = label_dict.encode(target[LABEL_KEY])
         labels.append(label)
         bbox = np.array(target[BBOX_KEY]).astype(np.int32).flatten().tolist()
+        bbox = convert24point(bbox)
         x = bbox[0::2]
         w = np.max(x) - np.min(x)
         y = bbox[1::2]
@@ -77,63 +86,53 @@ class GraphDataset(Dataset):
                  label: GraphLabel):
         self._ldict: GraphLabel = label
         self._adict: GraphAlphabet = alphabet
-        self._graphs: List = []
-        self._texts: List = []
-        self._lengths: List = []
-        self._labels: List = []
+        self._samples: List = []
         self._load(path)
+
+    def convert_data(self, sample):
+        bboxes, labels, texts, lengths = process(sample, self._ldict, self._adict)
+        node_size = labels.shape[0]
+        src: List = []
+        dst: List = []
+        dists: List = []
+        for i in range(node_size):
+            x_i = np.mean(bboxes[i][:8][0::2])
+            y_i = np.mean(bboxes[i][:8][1::2])
+            # w_i = bboxes[i][9]
+            # h_i = bboxes[i][8]
+            for j in range(node_size):
+                if i == j:
+                    continue
+
+                x_j = np.mean(bboxes[j][:8][0::2])
+                y_j = np.mean(bboxes[j][:8][1::2])
+                # h_j = bboxes[j][9]
+                # w_j = bboxes[j][8]
+                x_dist = x_j - x_i
+                y_dist = y_j - y_i
+
+                # if np.abs(y_dist) > 3 * h_j:
+                #     continue
+                dists.append([x_dist, y_dist, lengths[j] / lengths[i]])
+                src.append(i)
+                dst.append(j)
+        g = dgl.DGLGraph()
+        g.add_nodes(node_size)
+        g.add_edges(src, dst)
+        g.ndata['feat'] = torch.FloatTensor(norm(bboxes))
+        g.edata['feat'] = torch.FloatTensor(norm(np.array(dists)))
+        return g, texts, lengths, labels
 
     def _load(self, target_path: str):
         with open(target_path, 'r', encoding='utf-8') as f:
             samples: List = json.loads(remove_space(f.readline()))
-
-        for sample in samples:
-            bboxes, labels, texts, lengths = process(sample, self._ldict, self._adict)
-            node_size = labels.shape[0]
-            src: List = []
-            dst: List = []
-            dists: List = []
-            for i in range(node_size):
-                x_i = np.mean(bboxes[i][:8][0::2])
-                y_i = np.mean(bboxes[i][:8][1::2])
-                w_i = bboxes[i][9]
-                h_i = bboxes[i][8]
-                for j in range(node_size):
-                    if i == j:
-                        continue
-
-                    x_j = np.mean(bboxes[j][:8][0::2])
-                    y_j = np.mean(bboxes[j][:8][1::2])
-                    h_j = bboxes[j][9]
-                    w_j = bboxes[j][8]
-                    x_dist = x_j - x_i
-                    y_dist = y_j - y_i
-
-                    if np.abs(y_dist) > 3 * h_j:
-                        continue
-                    dists.append([x_dist, y_dist, lengths[j] / lengths[i]])
-                    src.append(i)
-                    dst.append(j)
-            g = dgl.DGLGraph()
-            g.add_nodes(node_size)
-            g.add_edges(src, dst)
-            g.ndata['feat'] = torch.FloatTensor(norm(bboxes))
-            g.edata['feat'] = torch.FloatTensor(norm(np.array(dists)))
-
-            self._graphs.append(g)
-            self._texts.append(texts)
-            self._lengths.append(lengths)
-            self._labels.append(labels)
+        self._samples.extend(samples)
 
     def __getitem__(self, index: int):
-        graph: dgl.DGLGraph = self._graphs[index]
-        text: np.ndarray = self._texts[index]
-        length: List = self._lengths[index]
-        label: int = self._labels[index]
-        return graph, label, text, length
+        return self.convert_data(self._samples[index])
 
     def __len__(self):
-        return len(self._graphs)
+        return len(self._samples)
 
 
 def get_factor(sizes: List) -> Tensor:
@@ -161,7 +160,6 @@ def graph_collate(batch: Tuple, pad_encode: int = 0):
     edge_factor: Tensor = get_factor(edge_sizes)
     batched_graph = dgl.batch(graphs)
 
-    print("okie lắm bạn ơi")
     return (batched_graph,
             torch.from_numpy(labels),
             torch.from_numpy(texts),
