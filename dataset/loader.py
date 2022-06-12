d5ceaf7ddb0c1986473e4aa2a605c991d12f47dc
Trinh_Thinh <Trinhthinh2171999@gmail.com>
2022-06-12 11:09:43 +0700

    initial

--- dataset/loader.py
+++ dataset/loader.py
@@ -102,12 +102,12 @@ class GraphDataset(Dataset):
         dst: List = []
         dists: List = []
         for i in range(node_size):
-            x_i, y_i, w_i, h_i = bboxes[i]
+            x_i, y_i, w_i, h_i, r_j = bboxes[i]
             for j in range(node_size):
                 if i == j:
                     continue
 
-                x_j, y_j, w_j, h_j = bboxes[j]
+                x_j, y_j, w_j, h_j, r_j = bboxes[j]
                 # h_j = bboxes[j][9]
                 x_dist = x_j - x_i
                 y_dist = y_j - y_i
